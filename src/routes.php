6ac3ea5ae5014645d0e88fb43fcd91a70cf507bc
USER-20160806QE\Administrator <874226876@qq.com>
2017-06-12 15:57:10 +0800

    点餐的前端页面模版

--- src/routes.php
+++ src/routes.php
@@ -1,27 +1,42 @@
 <?php
 // Routes
 
-$app->get('/hello/[{name}]', function ($request, $response, $args) {
-    // Sample log message
-    $this->logger->info("Slim-Skeleton '/' route");
-    // Render index view
+// 首页
+$app->get('/', function ($request, $response, $args) {
     return $this->renderer->render($response, 'index.phtml', $args);
 });
 
+// 点击首页的餐品，进入餐品详细页面
+$app->get('/detail/[{id}]', function ($request, $response, $args) {
+
+    var_dump($args);
+    return $this->renderer->render($response, 'detail.phtml', $args);
+});
+
+// 文章
+$app->get('/article', function ($request, $response, $args) {
+
+    return $this->renderer->render($response, 'article.phtml', $args);
+});
+
+// 关于我们店
+$app->get('/about', function ($request, $response, $args) {
+
+    return $this->renderer->render($response, 'about.phtml', $args);
+});
+//地图
+$app->get('/map', function ($request, $response, $args) {
+
+    return $this->renderer->render($response, 'map.phtml', $args);
+});
+
+
+// 生成的订单
+$app->get('/order', function ($request, $response, $args) {
+
+    return $this->renderer->render($response, 'order.phtml', $args);
+});
+
+// 后台的订单列表
 
 $app->get('/getTest', 'WidgetController:getTest');
-
-// 选择座位
-$app->get('/chooseSeat', function ($request, $response, $args) {
-
-
-    return $this->renderer->render($response, 'chooseSeat.phtml', $args);
-});
-
-//选择座位
-$app->get('/chooseSeat/[{id}]', function ($request, $response, $args) {
-
-    echo $args;
-    // Render index view
-    return $this->renderer->render($response, 'index.phtml', $args);
-});
